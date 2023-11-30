fdcdc7322de3c3d749c485835ea8b07032362f77
trollix <dr.chandra@laposte.net>
2023-11-30 11:01:21 +0100

    release 0.2.0

--- dist/tsmoon-card.js
+++ dist/tsmoon-card.js
@@ -124,6 +124,22 @@
                                 icon="mdi:lightbulb"
                             ></ha-icon>
                         </div>
+                        <div>
+
+                        <svg viewBox="0 0 36 36" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="iconify iconify--twemoji" preserveAspectRatio="xMidYMid meet">
+                            <circle fill="#FFD983" cx="18" cy="18" r="18"></circle>
+                            <path fill="#66757F" d="M36 18c0 9.941-8.059 18-18 18c-.294 0-.58-.029-.87-.043C11.239 33.393 7 26.332 7 18C7 9.669 11.239 2.607 17.13.044C17.42.03 17.706 0 18 0c9.941 0 18 8.059 18 18z"></path>
+                            <circle fill="#5B6876" cx="25.5" cy="8.5" r="3.5"></circle>
+                            <circle fill="#5B6876" cx="16" cy="16" r="3"></circle>
+                            <circle fill="#5B6876" cx="14.5" cy="27.5" r="3.5"></circle>
+                            <circle fill="#5B6876" cx="15" cy="6" r="2"></circle>
+                            <circle fill="#5B6876" cx="33" cy="18" r="1"></circle>
+                            <circle fill="#FFCC4D" cx="6" cy="9" r="1"></circle>
+                            <circle fill="#5B6876" cx="21" cy="31" r="1"></circle>
+                            <circle fill="#FFCC4D" cx="4" cy="19" r="2"></circle>
+                            <circle fill="#5B6876" cx="26" cy="23" r="2"></circle>
+                        </svg>
+        </div>
                         <div class="name truncate">
                             Entity name
                             <div class="secondary">Secondary info</div>
@@ -146,7 +162,7 @@
     ], TSMoonCard.prototype, "state", void 0);
 
     var name = "ha-tsmoon-card";
-    var version = "0.1.8";
+    var version = "0.2.0";
 
     const printVersionToConsole = () => console.info(`%c  ${name.toUpperCase()}  %c  Version ${version}  `, 'color: white; font-weight: bold; background: crimson', 'color: #000; font-weight: bold; background: #ddd');
 

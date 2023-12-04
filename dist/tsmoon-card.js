7ca240cf638dc08ea32adc9254e0f2139649a5f8
Trollix <trollix@gmail.com>
2023-12-04 20:32:46 +0100

    0.3.14

--- dist/tsmoon-card.js
+++ dist/tsmoon-card.js
@@ -225,9 +225,10 @@
          * @param config Card configuration (yaml converted to JSON)
          */
         setConfig(config) {
+            var _a;
             this.entity = config.entity;
             this.cardTitle = config.title || this.cardTitle;
-            this.icon_type !== undefined ? config.icon_type : 'forms';
+            this.icon_type = (_a = config.icon_type) !== null && _a !== void 0 ? _a : 'forms';
             console.log(config.icon_type);
             console.log(this.icon_type);
         }
@@ -270,7 +271,7 @@
     ], TSMoonCard.prototype, "icon_type", void 0);
 
     var name = "ha-tsmoon-card";
-    var version = "0.3.13";
+    var version = "0.3.14";
 
     const printVersionToConsole = () => console.info(`%c  ${name.toUpperCase()}  %c  Version ${version}  `, 'color: white; font-weight: bold; background: crimson', 'color: #000; font-weight: bold; background: #ddd');
 

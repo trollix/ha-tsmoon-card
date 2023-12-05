8b987a5e42a19ace55d930af9d09d97e1d919951
Trollix <trollix@gmail.com>
2023-12-05 23:12:22 +0100

    0.5.7

--- dist/tsmoon-card.js
+++ dist/tsmoon-card.js
@@ -547,7 +547,7 @@
     ], TSMoonCard.prototype, "config", void 0);
 
     var name = "ha-tsmoon-card";
-    var version = "0.5.6";
+    var version = "0.5.7";
 
     const printVersionToConsole = () => console.info(`%c  ${name.toUpperCase()}  %c  Version ${version}  `, 'color: white; font-weight: bold; background: crimson', 'color: #000; font-weight: bold; background: #ddd');
 

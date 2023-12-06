de3cd8afd76197a618c97a2f736164f7e9a081ea
trollix <dr.chandra@laposte.net>
2023-12-06 17:57:03 +0100

    release 0.5.19

--- dist/tsmoon-card.js
+++ dist/tsmoon-card.js
@@ -560,7 +560,7 @@
     ], TSMoonCard.prototype, "config", void 0);
 
     var name = "ha-tsmoon-card";
-    var version = "0.5.18";
+    var version = "0.5.19";
 
     const printVersionToConsole = () => console.info(`%c  ${name.toUpperCase()}  %c  Version ${version}  `, 'color: white; font-weight: bold; background: crimson', 'color: #000; font-weight: bold; background: #ddd');
 

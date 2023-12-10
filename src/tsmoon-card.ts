7755bd608500ef187195f0a2dc1db4bb8dc77a33
Trollix <trollix@gmail.com>
2023-12-10 23:46:49 +0100

    0.8.5

--- src/tsmoon-card.ts
+++ src/tsmoon-card.ts
@@ -124,11 +124,14 @@ export class TSMoonCard extends LitElement {
         const moonIcon = this.toIcon(this.state, this.icon_type);
         var l_state = this.localize(`moon.${this.state}`);
         const l_date = new Date();
-        console.log('Nom de fin de lune:', this.state);
+        //console.log('Phase de lune:', this.state);
         //this.getMoonRise();
 
         // Obtenez les temps du lever et du coucher du soleil
         const times = SunCalc.getMoonTimes(l_date, this.home_latitude, this.home_longitude);
+        
+        const moonRawData = SunCalc.getMoonData(l_date, this.home_latitude, this.home_longitude);
+        const moonPhase = Constants.MOON_PHASES[moonRawData.illumination.phase.id];
 
         // Accédez aux propriétés spécifiques pour obtenir les heures
         //const l_moonrise = times.rise;

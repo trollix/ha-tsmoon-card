4b8ff383d619ecf509996925550603c56adbd481
Trollix <trollix@gmail.com>
2025-04-05 19:01:56 +0200

    0.10.6

--- dist/tsmoon-card.js
+++ dist/tsmoon-card.js
@@ -2381,7 +2381,7 @@ var TSMoonCard = (function (exports) {
     ], TSMoonCard.prototype, "_config", void 0);
 
     var name = "ha-tsmoon-card";
-    var version = "v0.10.5";
+    var version = "v0.10.6";
 
     const printVersionToConsole = () => console.info(`%c  ${name.toUpperCase()}  %c  Version ${version}  `, 'color: white; font-weight: bold; background: crimson', 'color: #000; font-weight: bold; background: #ddd');
     // This puts your card into the UI card picker dialog
